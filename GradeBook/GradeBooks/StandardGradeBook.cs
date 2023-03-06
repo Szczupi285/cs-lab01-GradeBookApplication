1c09b8dae849926b8c6b9ac09108cb5ee8e882c2
Bartosz Bienięnda <bartosz.bienienda@microsoft.wsei.edu.pl>
2023-03-06 13:30:06 +0100

    Added 'Type' Property, Created StandardGradeBook Class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks 
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        string name { get; set; }
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+       
+    }
+}
